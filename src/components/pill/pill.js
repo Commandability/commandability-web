ce8df395cc028c8cdf87e6cdb7b956896ac056c4
Lee Morrow <leemorrow.dev@gmail.com>
2022-05-19 15:06:00 -0400

    Add spacer component

--- src/components/pill/pill.js
+++ src/components/pill/pill.js
@@ -1,24 +1,19 @@
 import * as React from "react";
 import styled from "styled-components";
 
-function Pill({children}){
-    return(
-        <>
-            <Button>{children}</Button>
-        </>
-    );
+function Pill({ children }) {
+  return <Button>{children}</Button>;
 }
 
 const Button = styled.button`
-  background-color: var( --color-yellow-9);
-  color: var( --color-yellow-2);
+  background-color: var(--color-yellow-9);
+  color: var(--color-yellow-2);
   border-radius: 24px;
   padding-top: 9px;
   padding-bottom: 9px;
   padding-left: 24px;
   padding-right: 24px;
   font-weight: bold;
-  font-size: 1rem;
 `;
 
 export default Pill;
