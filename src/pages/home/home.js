47931e85d0e4cb5eb136f59fa8cea73324f9bb6d
Lee Morrow <leemorrow.dev@gmail.com>
2022-05-23 13:35:29 -0400

    Add semantic html tags to home page
    Every page should have a main element per WAI-ARIA standards

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components";
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { auth } from "firebase.js";
 import { FiLogOut, FiLogIn } from "react-icons/fi";
@@ -14,37 +15,47 @@ function Home() {
   const [user] = useAuth();
 
   return (
-    <>
-      <Hero>
-        {user.current ? (
-          <Button onClick={() => signOut(auth)} theme="light" icon={FiLogOut}>
-            Sign out
-          </Button>
-        ) : (
-          <Button
-            onClick={() => signInWithEmailAndPassword(auth, "", "")}
-            theme="light"
-            icon={FiLogIn}
-          >
-            Sign in
-          </Button>
-        )}
-        <Spacer size={8} axis="vertical" />
-        <Pill theme="light" angle>
-          Learn more
-        </Pill>
-        <Spacer size={8} axis="vertical" />
-        <Pill theme="dark" angle>
-          Get started
-        </Pill>
-      </Hero>
-      <Footer>
-        <Pill theme="light" angle>
-          Contact us
-        </Pill>
-      </Footer>
-    </>
+    <Main>
+      <Section>
+        <Hero>
+          {user.current ? (
+            <Button onClick={() => signOut(auth)} theme="light" icon={FiLogOut}>
+              Sign out
+            </Button>
+          ) : (
+            <Button
+              onClick={() => signInWithEmailAndPassword(auth, "", "")}
+              theme="light"
+              icon={FiLogIn}
+            >
+              Sign in
+            </Button>
+          )}
+          <Spacer size={8} axis="vertical" />
+          <Pill theme="light" angle>
+            Learn more
+          </Pill>
+          <Spacer size={8} axis="vertical" />
+          <Pill theme="dark" angle>
+            Get started
+          </Pill>
+        </Hero>
+        <Footer>
+          <Pill theme="light" angle>
+            Contact us
+          </Pill>
+        </Footer>
+      </Section>
+    </Main>
   );
 }
+
+const Main = styled.main`
+  height: 100%;
+`;
+
+const Section = styled.section`
+  height: 100%;
+`;
 
 export default Home;
