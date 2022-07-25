2ea2735f16d4613f26285f68b0eee6c537a6c936
Noah Hughes <noah.hughes357@gmail.com>
2022-07-25 12:25:40 -0400

    Changed dialog form to only enclose form info
    Animated color shift on hover for submit
    
    animate dialog navigation, speed up

--- src/components/user-account-dialog/user-account-dialog.js
+++ src/components/user-account-dialog/user-account-dialog.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import { zeroRightClassName } from "react-remove-scroll-bar";
 import * as Dialog from "@radix-ui/react-dialog";
 
+import UnstyledButton from "components/unstyled-button";
+
 function UserAccountDialog(props) {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
@@ -46,7 +48,7 @@ function UserAccountDialog(props) {
     setIsAnimating(true);
     setTimeout(() => {
       setDialog(target);
-    }, "300");
+    }, "150");
   }
 
   function handleClose() {
@@ -67,7 +69,10 @@ function UserAccountDialog(props) {
       </AccountTrigger>
       <Dialog.Portal>
         <Overlay className={zeroRightClassName} />
-        <AccountContent onInteractOutside={handleClose}>
+        <AccountContent
+          onInteractOutside={handleClose}
+          className={zeroRightClassName}
+        >
           <AnimationControl
             className={isAnimating ? "animate" : ""}
             onAnimationEnd={() => setIsAnimating(false)}
@@ -78,51 +83,51 @@ function UserAccountDialog(props) {
               The className is part of the react-remove-scroll-bar package. This will make sure any fixed position elements will
               have their right position modified to match the original right position before the scroll bar is removed.
              */}
-                <ContentContainer className={zeroRightClassName}>
-                  <AllInputsWrapper>
-                    <InputWrapper>
-                      <Label>Display Name</Label>
-                      <Input
-                        id="newDisplayName"
-                        type="text"
-                        placeholder="Department One"
-                        onChange={handleNewDisplayNameChange}
-                        value={newDisplayName}
-                      />
-                    </InputWrapper>
-                    <InputWrapper>
-                      <Label>Email</Label>
-                      <Input
-                        id="newEmailInput"
-                        type="email"
-                        placeholder="example@example.com"
-                        onChange={handleNewEmailChange}
-                        value={newEmail}
-                      />
-                    </InputWrapper>
-                    <InputWrapper>
-                      <Label>Password</Label>
-                      <Input
-                        id="newPasswordInput"
-                        type="password"
-                        placeholder="example password"
-                        onChange={handleNewPasswordChange}
-                        value={newPassword}
-                      />
-                    </InputWrapper>
-                  </AllInputsWrapper>
+
+                <AccountForm>
+                  <InputWrapper>
+                    <Label htmlFor="newDisplayName">Display Name</Label>
+                    <Input
+                      id="newDisplayName"
+                      type="text"
+                      placeholder="Department One"
+                      onChange={handleNewDisplayNameChange}
+                      value={newDisplayName}
+                    />
+                  </InputWrapper>
+                  <InputWrapper>
+                    <Label htmlFor="newEmailInput">Email</Label>
+                    <Input
+                      id="newEmailInput"
+                      type="email"
+                      placeholder="example@example.com"
+                      onChange={handleNewEmailChange}
+                      value={newEmail}
+                    />
+                  </InputWrapper>
+                  <InputWrapper>
+                    <Label htmlFor="newPasswordInput">Password</Label>
+                    <Input
+                      id="newPasswordInput"
+                      type="password"
+                      placeholder="example password"
+                      onChange={handleNewPasswordChange}
+                      value={newPassword}
+                    />
+                  </InputWrapper>
+
                   <SubmitButton type="submit" onClick={handleNewAccountSubmit}>
                     Create Account
                   </SubmitButton>
-                  <OtherNavOptions>
-                    <hr />
-                    <DialogSwitchButton
-                      onClick={() => handleNavClick("CurrentUser")}
-                    >
-                      Already have an account?
-                    </DialogSwitchButton>
-                  </OtherNavOptions>
-                </ContentContainer>
+                </AccountForm>
+                <OtherNavOptions>
+                  <hr />
+                  <DialogSwitchButton
+                    onClick={() => handleNavClick("CurrentUser")}
+                  >
+                    Already have an account?
+                  </DialogSwitchButton>
+                </OtherNavOptions>
               </>
             ) : dialog === "CurrentUser" ? (
               <>
@@ -130,46 +135,44 @@ function UserAccountDialog(props) {
               The className is part of the react-remove-scroll-bar package. This will make sure any fixed position elements will
               have their right position modified to match the original right position before the scroll bar is removed.
              */}
-                <ContentContainer className={zeroRightClassName}>
-                  <AllInputsWrapper>
-                    <InputWrapper>
-                      <Label>Email</Label>
-                      <Input
-                        id="emailInput"
-                        type="email"
-                        placeholder="example@example.com"
-                        onChange={handleEmailChange}
-                        value={email}
-                      />
-                    </InputWrapper>
-                    <InputWrapper>
-                      <Label>Password</Label>
-                      <Input
-                        id="passwordInput"
-                        type="password"
-                        placeholder="example password"
-                        onChange={handlePasswordChange}
-                        value={password}
-                      />
-                    </InputWrapper>
-                  </AllInputsWrapper>
+
+                <AccountForm>
+                  <InputWrapper>
+                    <Label htmlFor="emailInput">Email</Label>
+                    <Input
+                      id="emailInput"
+                      type="email"
+                      placeholder="example@example.com"
+                      onChange={handleEmailChange}
+                      value={email}
+                    />
+                  </InputWrapper>
+                  <InputWrapper>
+                    <Label htmlFor="passwordInput">Password</Label>
+                    <Input
+                      id="passwordInput"
+                      type="password"
+                      placeholder="example password"
+                      onChange={handlePasswordChange}
+                      value={password}
+                    />
+                  </InputWrapper>
+
                   <SubmitButton type="submit" onClick={handleLoginSubmit}>
                     Login
                   </SubmitButton>
-                  <ForgotPasswordButton
-                    onClick={() => handleNavClick("ForgotPassword")}
-                  >
-                    Forgot password?
-                  </ForgotPasswordButton>
-                  <OtherNavOptions>
-                    <hr />
-                    <DialogSwitchButton
-                      onClick={() => handleNavClick("NewUser")}
-                    >
-                      Need an account?
-                    </DialogSwitchButton>
-                  </OtherNavOptions>
-                </ContentContainer>
+                </AccountForm>
+                <ForgotPasswordButton
+                  onClick={() => handleNavClick("ForgotPassword")}
+                >
+                  Forgot password?
+                </ForgotPasswordButton>
+                <OtherNavOptions>
+                  <hr />
+                  <DialogSwitchButton onClick={() => handleNavClick("NewUser")}>
+                    Create an account
+                  </DialogSwitchButton>
+                </OtherNavOptions>
               </>
             ) : (
               <>
@@ -177,33 +180,33 @@ function UserAccountDialog(props) {
               The className is part of the react-remove-scroll-bar package. This will make sure any fixed position elements will
               have their right position modified to match the original right position before the scroll bar is removed.
              */}
-                <ContentContainer className={zeroRightClassName}>
-                  <AllInputsWrapper>
-                    <InputWrapper>
-                      <Label>Email</Label>
-                      <Input
-                        id="emailInput"
-                        type="email"
-                        placeholder="example@example.com"
-                        onChange={handleEmailChange}
-                        value={email}
-                      />
-                    </InputWrapper>
-                  </AllInputsWrapper>
+
+                <AccountForm>
+                  <InputWrapper>
+                    <Label htmlFor="emailInput">Email</Label>
+                    <Input
+                      id="emailInput"
+                      type="email"
+                      placeholder="example@example.com"
+                      onChange={handleEmailChange}
+                      value={email}
+                    />
+                  </InputWrapper>
+
                   <Dialog.Close asChild>
                     <SubmitButton type="submit" onClick={handleClose}>
                       Reset Password
                     </SubmitButton>
                   </Dialog.Close>
-                  <OtherNavOptions>
-                    <hr />
-                    <DialogSwitchButton
-                      onClick={() => handleNavClick("CurrentUser")}
-                    >
-                      Click here to return to login
-                    </DialogSwitchButton>
-                  </OtherNavOptions>
-                </ContentContainer>
+                </AccountForm>
+                <OtherNavOptions>
+                  <hr />
+                  <DialogSwitchButton
+                    onClick={() => handleNavClick("CurrentUser")}
+                  >
+                    Return to login
+                  </DialogSwitchButton>
+                </OtherNavOptions>
               </>
             )}
           </AnimationControl>
@@ -213,28 +216,12 @@ function UserAccountDialog(props) {
   );
 }
 
-const ContentContainer = styled.form`
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  margin: auto;
-  height: 540px;
-  width: 480px;
-  background-color: var(--color-gray-10);
-  border-radius: 16px;
-  padding: 36px;
-  box-shadow: var(--box-shadow);
-`;
-
-const AllInputsWrapper = styled.div`
+const AccountForm = styled.form`
   display: flex;
   position: relative;
   gap: 24px;
   flex-direction: column;
   width: 100%;
-  padding-bottom: 36px;
 `;
 
 const InputWrapper = styled.div`
@@ -264,8 +251,9 @@ const Input = styled.input`
   }
 `;
 
-const SubmitButton = styled.button`
+const SubmitButton = styled(UnstyledButton)`
   padding: 12px;
+  margin-top: 12px;
   width: 100%;
   background-color: var(--color-red-3);
   border: none;
@@ -275,17 +263,30 @@ const SubmitButton = styled.button`
   font-weight: bold;
   letter-spacing: 0.5px;
   text-transform: uppercase;
+  text-align: center;
+
+  &:focus {
+    outline-offset: 3px;
+  }
   &:hover {
-    background-color: var(--color-red-2);
     cursor: pointer;
+    animation: morph 200ms linear;
+    background-color: var(--color-yellow-3);
+  }
+  @keyframes morph {
+    0% {
+      background-color: var(--color-red-3);
+    }
+    100% {
+      background-color: var(--color-yellow-3);
+    }
   }
 `;
 
-const ForgotPasswordButton = styled.div`
+const ForgotPasswordButton = styled(UnstyledButton)`
   color: var(--color-yellow-2);
   font-weight: bold;
-  padding-top: 12px;
-  text-align: center;
+  margin: auto;
   &:hover {
     color: var(--color-yellow-4);
     cursor: pointer;
@@ -300,11 +301,11 @@ const OtherNavOptions = styled.div`
   padding: 36px;
 `;
 
-const DialogSwitchButton = styled.div`
+const DialogSwitchButton = styled(UnstyledButton)`
   color: var(--color-yellow-2);
   font-weight: bold;
-  text-align: center;
   padding-top: 12px;
+  margin: auto;
   &:hover {
     color: var(--color-yellow-4);
     cursor: pointer;
@@ -314,7 +315,7 @@ const DialogSwitchButton = styled.div`
 const AnimationControl = styled.div`
   @media (prefers-reduced-motion: no-preference) {
     &.animate {
-      animation: fade 600ms ease-in-out;
+      animation: fade 300ms ease-in-out;
     }
   }
   @keyframes fade {
@@ -331,14 +332,18 @@ const AnimationControl = styled.div`
 `;
 
 const AccountContent = styled(Dialog.Content)`
-  position: absolute;
+  position: fixed;
   top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   margin: auto;
-  width: fit-content;
-  height: fit-content;
+  height: 540px;
+  width: 480px;
+  background-color: var(--color-gray-10);
+  border-radius: 16px;
+  padding: 36px;
+  box-shadow: var(--box-shadow);
   z-index: 1000;
 
   @media (prefers-reduced-motion: no-preference) {
