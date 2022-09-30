6435dfa2f60dce8984499f1355034e807df7e088
Lee Morrow <leemorrow.dev@gmail.com>
2022-09-30 17:26:27 -0400

    Add text button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -7,42 +7,85 @@ const THEMES = {
     "--background-color": "var(--color-yellow-9)",
     "--hover-background-color": "var(--color-yellow-8)",
     "--color": "var(--color-yellow-2)",
+    "--hover-color": "var(--color-yellow-3)",
   },
   dark: {
     "--background-color": "var(--color-yellow-2)",
     "--hover-background-color": "var(--color-yellow-3)",
     "--color": "var(--color-yellow-9)",
+    "--hover-color": "var(--color-yellow-8)",
     "--selection-color": "var(--color-yellow-3)",
     "--selection-background": "var(--color-yellow-10)",
   },
 };
 
 const Button = React.forwardRef(
-  ({ theme, icon, children, style, ...props }, forwardedRef) => {
+  (
+    { theme = "light", type = "solid", icon, children, style, ...props },
+    forwardedRef
+  ) => {
     const themeStyles = THEMES[theme];
 
-    return (
-      <ButtonBase
-        {...props}
-        ref={forwardedRef}
-        style={{ ...themeStyles, ...style }}
-      >
-        {icon ? icon() : null}
-        {icon ? <Spacer size={8} axis="horizontal" /> : null}
-        <Text>{children}</Text>
-      </ButtonBase>
-    );
+    if (type === "text") {
+      return (
+        <TextButton
+          {...props}
+          ref={forwardedRef}
+          style={{ ...themeStyles, ...style }}
+        >
+          {children}
+        </TextButton>
+      );
+    } else {
+      return (
+        <SolidButton
+          {...props}
+          ref={forwardedRef}
+          style={{ ...themeStyles, ...style }}
+        >
+          {icon ? icon() : null}
+          {icon ? <Spacer size={8} axis="horizontal" /> : null}
+          <Text>{children}</Text>
+        </SolidButton>
+      );
+    }
   }
 );
 
 const ButtonBase = styled.button`
   display: flex;
   align-items: center;
-  padding: 8px 16px;
-  border-radius: 8px;
   border: none;
   cursor: pointer;
   -webkit-tap-highlight-color: transparent;
+  letter-spacing: 0.05em;
+`;
+
+const TextButton = styled(ButtonBase)`
+  padding: 0;
+  background-color: transparent;
+  font-weight: bold;
+  text-transform: uppercase;
+  color: var(--color);
+
+  &:focus {
+    outline-offset: 2px;
+  }
+
+  &:active {
+    color: var(--hover-color);
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      color: var(--hover-color);
+    }
+  }
+`;
+
+const SolidButton = styled(ButtonBase)`
+  padding: 8px 16px;
+  border-radius: 8px;
   background-color: var(--background-color);
   font-weight: bold;
   text-transform: uppercase;
