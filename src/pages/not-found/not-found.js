af2b6ae2b1e4367296dba97666d67cb5d0b7d48a
Lee Morrow <leemorrow.dev@gmail.com>
2022-10-17 16:29:17 -0400

    Format not found page

--- src/pages/not-found/not-found.js
+++ src/pages/not-found/not-found.js
@@ -6,13 +6,25 @@ import Layout from "components/layout";
 function NotFound() {
   return (
     <Layout>
-      <Text>404: Not Found</Text>
+      <Wrapper>
+        <Text>404: Not Found</Text>
+      </Wrapper>
     </Layout>
   );
 }
 
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: grid;
+  place-content: center;
+`;
+
 const Text = styled.div`
+  font-size: ${72 / 16}rem;
   color: var(--color-white);
+  text-transform: uppercase;
+  letter-spacing: 0.05em;
 `;
 
 export default NotFound;
