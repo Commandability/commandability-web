ae67f145ca77f94bbcf7c837f80ad305396e14ca
Lee Morrow <leemorrow.dev@gmail.com>
2022-10-26 10:38:07 -0400

    Add report item

---
+++ src/components/report-item/report-item.js
@@ -0,0 +1,124 @@
+import * as React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import Checkbox from "components/checkbox";
+
+function ReportItem({
+  setCheckedItems,
+  checkedAll,
+  setCheckedAll,
+  reportId,
+  location,
+  startTimestamp,
+  ...props
+}) {
+  const { status, origin } = checkedAll;
+
+  const [checked, setChecked] = React.useState(false);
+
+  React.useEffect(() => {
+    // Check item when checkedAll is checked
+    if (status) {
+      setChecked(true);
+      setCheckedItems((checkedItems) => [...checkedItems, reportId]);
+      // Uncheck item when checkedAll is unchecked, but not when a single person has been unchecked
+    } else if (origin !== "list") {
+      setChecked(false);
+      setCheckedItems((checkedItems) =>
+        checkedItems.filter((item) => item !== reportId)
+      );
+    }
+  }, [status, origin, setCheckedItems, reportId]);
+
+  return (
+    <Wrapper data-checked={checked ? "true" : "false"} {...props}>
+      <PositionedCheckbox
+        label="Select"
+        checked={checked}
+        onCheckedChange={(checked) => {
+          setChecked(checked);
+
+          if (checked) {
+            setCheckedItems((checkedItems) => [...checkedItems, reportId]);
+          } else {
+            setCheckedItems((checkedItems) =>
+              checkedItems.filter((item) => item !== reportId)
+            );
+
+            // Disable checked all when a single person has been unchecked
+            setCheckedAll({ status: false, origin: "list" });
+          }
+        }}
+      />
+      <Contents>
+        <Group>
+          <Location to={`/dashboard/reports/${reportId}`}>{location}</Location>
+        </Group>
+        <span>
+          {startTimestamp.toDate().toLocaleString(navigator.language, {
+            year: "2-digit",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </span>
+      </Contents>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.li`
+  display: flex;
+  padding: 16px 48px;
+  border-bottom: 1px solid var(--color-gray-9);
+  background-color: var(--color-white);
+  color: var(--color-gray-3);
+  // Prevent height: 1px and position: absolute in VisuallyHidden in CheckBox
+  // from stacking outside root html element and causing vertical overflow
+  // Also add name pseudo-element containing position
+  position: relative;
+
+  &[data-checked="true"] {
+    background-color: var(--color-gray-8);
+  }
+
+  @media (hover: hover) and (pointer: fine) {
+    &:hover {
+      background-color: var(--color-gray-8);
+    }
+  }
+`;
+
+const PositionedCheckbox = styled(Checkbox)`
+  // Move checkbox out of flow and place above after pseudo-element
+  position: absolute;
+  z-index: 1;
+`;
+
+const Contents = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: space-between;
+  // Account for width of absolutely positioned checkbox and gap
+  padding-left: calc(24px + 32px);
+`;
+
+const Group = styled.div`
+  display: flex;
+  gap: 32px;
+`;
+
+const Location = styled(Link)`
+  text-decoration: none;
+  color: var(--color-gray-3);
+
+  &::after {
+    content: "";
+    position: absolute;
+    inset: 0;
+  }
+`;
+
+export default ReportItem;
