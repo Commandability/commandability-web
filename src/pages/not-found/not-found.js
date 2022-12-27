bfbe93c851ad5207be56ddd83ed3a21e7bafb16f
Lee Morrow <leemorrow.dev@gmail.com>
2022-12-27 17:01:05 -0500

    Improve not found page

--- src/pages/not-found/not-found.js
+++ src/pages/not-found/not-found.js
@@ -2,12 +2,21 @@ import * as React from "react";
 import styled from "styled-components";
 
 import Layout from "components/layout";
+import Pill from "components/pill";
+import Spacer from "components/spacer";
 
 function NotFound() {
   return (
     <Layout>
       <Wrapper>
-        <Text>404: Not Found</Text>
+        <Content>
+          <Code>404</Code>
+          <Message>Page not found</Message>
+          <Spacer axis="vertical" size={64} />
+          <Pill to="/" theme="light" angle>
+            Go home
+          </Pill>
+        </Content>
       </Wrapper>
     </Layout>
   );
@@ -16,12 +25,28 @@ function NotFound() {
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
+  padding: 0 48px;
   display: grid;
-  place-content: center;
+  grid-template-rows: repeat(4, 1fr);
+  justify-content: center;
 `;
 
-const Text = styled.div`
-  font-size: ${72 / 16}rem;
+const Content = styled.div`
+  grid-row: 2;
+  display: grid;
+  justify-items: start;
+`;
+
+const Code = styled.div`
+  display: grid;
+  font-size: clamp(${32 / 16}rem, 8vw + 1rem, ${96 / 16}rem);
+  letter-spacing: 0.1em;
+  color: var(--color-yellow-9);
+`;
+
+const Message = styled.div`
+  display: grid;
+  font-size: clamp(${24 / 16}rem, 2vw + 1rem, ${48 / 16}rem);
   color: var(--color-white);
   text-transform: uppercase;
   letter-spacing: 0.05em;
