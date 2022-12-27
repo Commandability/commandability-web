641d67c3bc055b0e01e2ad2346f9988cb87dce12
Lee Morrow <leemorrow.dev@gmail.com>
2022-12-27 09:25:56 -0500

    Update utils types

--- src/utils.ts
+++ src/utils.ts
@@ -8,7 +8,7 @@
  * @param max - The maximum integer
  * @returns A random integer between min and max
  */
-export function getRandomInt(min: number, max: number) {
+export function getRandomInt(min: number, max: number): number {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
