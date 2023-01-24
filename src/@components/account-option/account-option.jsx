be0917e9267bb0c6e586c8dab08c7305d1ec1aec
Noah Hughes <noah.hughes357@gmail.com>
2023-01-24 15:00:51 -0500

    fix layout issues on account page

--- src/@components/account-option/account-option.jsx
+++ src/@components/account-option/account-option.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-function AccountOption({ header, children }) {
+function AccountOption({ header, children, layout = "vertical" }) {
   return (
-    <Wrapper>
+    <Wrapper layout={layout}>
       <Header>{header}</Header>
       <Content>{children}</Content>
     </Wrapper>
@@ -11,27 +11,31 @@ function AccountOption({ header, children }) {
 }
 
 const Wrapper = styled.div`
+  position: relative;
   display: flex;
-  flex-direction: column;
+  flex-direction: ${(props) =>
+    props.layout === "horizontal" ? "row" : "column"};
   background-color: var(--color-gray-10);
   border-radius: 8px;
   height: fit-content;
   width: 640px;
   padding: 24px;
+  gap: 24px;
+  align-items: ${(props) =>
+    props.layout === "horizontal" ? "baseline" : null};
 `;
 
-const Header = styled.div`
+const Header = styled.header`
   flex: 1;
   font-weight: bold;
   text-transform: uppercase;
   font-size: ${16 / 16}rem;
   color: var(--color-gray-3);
-  margin-bottom: 24px;
   letter-spacing: 0.05em;
 `;
 
 const Content = styled.div`
-  flex: 3;
+  flex: 1;
   display: flex;
   flex-direction: column;
   justify-self: flex-end;
