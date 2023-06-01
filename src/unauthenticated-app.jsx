1db19030215e8943efbc17ab74c1f66490a00baa
Noah Hughes <noah.hughes357@gmail.com>
2023-06-01 16:10:21 -0400

    Rename manage-account to message

--- src/unauthenticated-app.jsx
+++ src/unauthenticated-app.jsx
@@ -10,7 +10,7 @@ import Layout from "@components/layout";
 import Home from "@pages/home";
 import NotFound from "@pages/not-found";
 import PrivacyPolicy from "@pages/privacy-policy";
-import ManageAccount from "@pages/manage-account";
+import Message from "@pages/message";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,7 +18,7 @@ const router = createBrowserRouter(
       <Route path="/" element={<Home />} errorElement={<NotFound />} />
       <Route element={<Layout type="outlet" />} errorElement={<NotFound />}>
         <Route path="privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="manage-account" element={<ManageAccount />} />
+        <Route path="message" element={<Message />} />
       </Route>
     </>
   )
